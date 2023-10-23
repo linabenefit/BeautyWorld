131a3a7dac4b02d8eccf2bd3691dac830bb15677
Lina Gaysina <lin.gaysina@vk.com>
2023-10-23 11:09:17 +0700

    Smooth scroll

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,14 @@ $(document).ready(function(){
 	$('.header__burger').click(function(event){
         $('.header__burger,.navigation').toggleClass('active')
         $('body').toggleClass('lock');
+ 
+    });
+});
 
+$(document).ready(function() {
+    $('.navigation-item').click(function(event) {
+        $('.header__burger,.navigation').removeClass('active');
+        $('body').removeClass('lock')
     });
 });
 
@@ -31,7 +38,28 @@ function retrieveFormValue(event){
 
 
 
+/*SLIDER*/
+
+new Swiper('.image-slider'), {
+    
+    navigation: {
+    nextEl:'swiper-button-next',
+    prevEl:'swiper-button-prev'
+    },
+
+    slidesPerView: 4,
+    
+}
 
 
+/*SMOOTH SCROLL*/
 
+$('a[href^="#"]').on('click', function() {
 
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
