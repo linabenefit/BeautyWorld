ffe57d84ff0273484c1b7bec2e52560add956f27
Lina Gaysina <lin.gaysina@vk.com>
2023-10-26 23:44:24 +0700

    Fix Prettier and add Webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,58 @@
-
+console.log("webpack");
 
 /* BURGER */
 
-$(document).ready(function(){
-	$('.header__burger').click(function(event){
-        $('.header__burger,.navigation').toggleClass('active')
-        $('body').toggleClass('lock');
- 
-    });
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.navigation").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
 });
 
-$(document).ready(function() {
-    $('.navigation-item').click(function(event) {
-        $('.header__burger,.navigation').removeClass('active');
-        $('body').removeClass('lock')
-    });
+$(document).ready(function () {
+  $(".navigation-item").click(function (event) {
+    $(".header__burger,.navigation").removeClass("active");
+    $("body").removeClass("lock");
+  });
 });
-
 
 /* FORM */
 
-const form = document.getElementById('form');
+const form = document.getElementById("form");
 
-function retrieveFormValue(event){
-    event.preventDefault();
-    
-    const Username = form.querySelector('[name="Username"]'), 
+function retrieveFormValue(event) {
+  event.preventDefault();
+
+  const Username = form.querySelector('[name="Username"]'),
     Usertel = form.querySelector('[name="Usertel"]');
 
-    const values = {
-        Username: Username.value,
-        Usertel: Usertel.value
-    };
+  const values = {
+    Username: Username.value,
+    Usertel: Usertel.value,
+  };
 
-    console.log ('Получение данных',values);
+  console.log("Получение данных", values);
 }
-
-
 
 /*SLIDER*/
 
-new Swiper('.image-slider'), {
-    
+new Swiper(".image-slider"),
+  {
     navigation: {
-    nextEl:'swiper-button-next',
-    prevEl:'swiper-button-prev'
+      nextEl: "swiper-button-next",
+      prevEl: "swiper-button-prev",
     },
 
     slidesPerView: 4,
-    
-}
-
+  };
 
 /*SMOOTH SCROLL*/
 
-$('a[href^="#"]').on('click', function() {
+$('a[href^="#"]').on("click", function () {
+  let href = $(this).attr("href");
 
-    let href = $(this).attr('href');
-
-    $('html, body').animate({
-        scrollTop: $(href).offset().top
-    });
-    return false;
+  $("html, body").animate({
+    scrollTop: $(href).offset().top,
+  });
+  return false;
 });
